f09e34ce6f6d1e0759742f8d2ad3e6f7b2ebe0da
남수진 <a25062401@gmail.com>
2023-11-12 16:08:44 +0900

    feat: spring 프로젝트 생성

---
+++ triptape/src/main/java/com/ssafy/triptape/TriptapeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.triptape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriptapeApplication.class, args);
+	}
+
+}
---
+++ triptape/src/test/java/com/ssafy/triptape/TriptapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.triptape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
